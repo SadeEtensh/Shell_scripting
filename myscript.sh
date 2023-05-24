1678eca37482267f12e4298f737dd5609828469c
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-24 10:16:02 +0300

    if Else statement

--- myscript.sh
+++ myscript.sh
@@ -2,9 +2,36 @@
 
 
 #VARIABLES
-#NAME="Sade"
+NAME="Jack"
 
 #echo "My Name is $NAME"
 
-read -p "Enter Name"  NAME
-echo " My name is $NAME"
+# read -p "Enter Name"  NAME
+# echo " My name is $NAME"
+
+# SIMPLE IF STATMENT
+
+# if [ "$NAME" == "Sade" ]
+# then
+#     echo "Your Name is Sade"
+# fi
+
+#IF ELSE
+
+# if [ "$NAME" == "Sade" ]
+# then
+#     echo "Your Name is Sade"
+# else
+#     echo "Your name is not Sade"
+# fi
+
+#ELSE-IF (elif)
+if [ "$NAME" == "Sade" ]
+then
+    echo "Your Name is Sade"
+elif [ "$NAME" == "Jack" ]
+then
+    echo "Your name is JacK"
+else
+    echo "Your name is not Sade or Jack"
+fi
