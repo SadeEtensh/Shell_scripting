b4e24cc72eda15779051097930ac57cd0ca9c69d
SadeEtensh <eteneshsade550305@gmail.com>
2023-05-24 11:05:51 +0300

    loops, functions...

--- myscript.sh
+++ myscript.sh
@@ -79,18 +79,18 @@
 
 #CASE STATEMENTS
 
-# read -p "Are U 21 or over? Y/N " ANSWER
-# case "$ANSWER" in
-#  [yY] | [yY][eE][sS])
-#     echo "You can have a beer :) "
-#     ;;
-#  [nN] | [nN][oO])
-#     echo "Sorry, No drinking"
-#     ;;
-#  *)
-#   echo "Please enter y/yes or n/No"
-#   ;;
-#   esac
+read -p "Are U 21 or over? Y/N " ANSWER
+case "$ANSWER" in
+ [yY] | [yY][eE][sS])
+    echo "You can have a beer :) "
+    ;;
+ [nN] | [nN][oO])
+    echo "Sorry, No drinking"
+    ;;
+ *)
+  echo "Please enter y/yes or n/No"
+  ;;
+  esac
 
 #LOOPS
 # NAMES="Sade Mark Jony Iris"
@@ -109,3 +109,30 @@ for FILE in $FILES
     echo "Renaming $FILE to new-$FILE"
     mv $FILE $NEW-$FILE
 done
+
+# WHILE LOOP - READ THROUGH A FILE LINE BY LINE
+LINE=1
+while read -r CURRENT_LINE
+  do
+    echo "$LINE: $CURRENT_LINE"
+    ((LINE++))
+done < "./new-1.txt"
+
+# FUNCTION
+function sayHello() {
+  echo "Hello World"
+}
+# sayHello
+
+# FUNCTION WITH PARAMS
+# function greet() {
+#   echo "Hello, I am $1 and I am $2"
+# }
+
+# greet "Brad" "36"
+
+# CREATE FOLDER AND WRITE TO A FILE
+# mkdir hello
+# touch "hello/world.txt"
+# echo "Hello World" >> "hello/world.txt"
+# echo "Created hello/world.txt"
